07b39dcb67483419faee0904b68c64880a89ab49
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 21:36:53 +0500

    Add examples of variable usage in Python

--- Practice1/python-basics/variables.py
+++ Practice1/python-basics/variables.py
@@ -1,1 +1,28 @@
+#1 example
+x = 5
+y = "Dilnara"
+print(x)
+print(y)
+
+#2 example
+x = 5
+print(type(x))
+
+#3 example
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#4 example
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#5 example
+x = 5
+y = 10
+print(x + y)
 
