147068c7f932529968d87fa887d24a69f05ead81
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 23:17:03 +0500

    Enhance boolean_comparison.py with variable examples
    Added boolean comparison examples using variables and expressions.

--- Practice2/boolean/boolean_comparison.py
+++ Practice2/boolean/boolean_comparison.py
@@ -1,30 +1,16 @@
 #1
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
 
 #2
-a = 200
-b = 33
+x = 5
+print(1 < x < 10)
+print(1 < x and x < 10)
 
-if b > a:
-  print("b is greater than a")
-else:
-  print("b is not greater than a")
-
-#3
-print(bool("Hello"))
-print(bool(15))
-
-#4
-x = "Hello"
-y = 15
-
-print(bool(x))
-print(bool(y))
-
-#5
-def myFunction() :
-  return True
-
-print(myFunction())
