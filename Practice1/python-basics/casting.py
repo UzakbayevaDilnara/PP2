283ea10950fed711159aee1ed1e696f9cc4de55a
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 21:46:53 +0500

    Add examples of type casting in Python

--- Practice1/python-basics/casting.py
+++ Practice1/python-basics/casting.py
@@ -1,1 +1,23 @@
+#1 example
+x = int(1)   
+y = int(2.8) 
+z = int("3") 
+
+#2 example
+x = str("s1") 
+y = str(2)    
+z = str(3.0)
+
+#3 example
+x = float(1)    
+y = float(2.8) 
+z = float("3")  
+w = float("4.2") 
+
+#4 example
+print(int(35.88))
+
+#5 example
+print(float(38))
+
 
