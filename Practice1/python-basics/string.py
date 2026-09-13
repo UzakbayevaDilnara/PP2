aedb44cfbeb41a2e231e87a407e0c2dc4fc3ee85
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 21:53:14 +0500

    Update string.py

--- Practice1/python-basics/string.py
+++ Practice1/python-basics/string.py
@@ -1,1 +1,14 @@
-
+#1 example
+print("It's alright")
+#2 example
+a = "Hello"
+print(a)
+#3 example
+a = "Hello, World!"
+print(a.upper())
+#4 example
+a = "Hello, World!"
+print(a[1])
+#5 example
+a = "Hello, World!"
+print(len(a))
