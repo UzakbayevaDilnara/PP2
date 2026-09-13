d37dbab5eff12279ae19675bdea8f382de7d2a2a
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 21:44:11 +0500

    Add examples of number types in Python

--- Practice1/python-basics/numbers.py
+++ Practice1/python-basics/numbers.py
@@ -1,1 +1,33 @@
+#1 example
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
 
+#2 example
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3 example
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4 example
+import random
+print(random.randrange(1, 10))
+
+#5 example
+x = 1
+y = 3.14
+print(type(x))
+print(type(y))
