4f7a753d6ec84316a35863d549f819e634f51eef
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 23:24:02 +0500

    Update boolean_operators.py

--- Practice2/boolean/boolean_operators.py
+++ Practice2/boolean/boolean_operators.py
@@ -1,1 +1,29 @@
+#1
+x = 5
+print(x > 0 and x < 10)
 
+#2
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#3
+x = 12
+y = 5
+print(x / y)
+
+#4
+x = 12
+y = 5
+print(x // y)
+
+#5
+sum1 = 100 + 50      
+sum2 = sum1 + 250    
+sum3 = sum2 + sum2
