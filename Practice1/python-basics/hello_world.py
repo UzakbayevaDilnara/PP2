b25d11c39eceaed4226b9912b1ab9d83b770a4af
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-13 21:32:58 +0500

    Update hello_world.py

--- Practice1/python-basics/hello_world.py
+++ Practice1/python-basics/hello_world.py
@@ -1,1 +1,10 @@
-
+#1 example
+print("Hello, World!")
+#2 example
+print("hi, Dilnara")
+#3 example
+print("I am learning Python")
+#4 example
+print("This will work!")
+#5 example
+print("I will print on the same line.")
