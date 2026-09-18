7188c53497d54f41238163724f9fb0c0702f40cb
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-19 01:08:53 +0500

    Update class_variables.py

--- Practice3/classes/class_variables.py
+++ Practice3/classes/class_variables.py
@@ -1,1 +1,9 @@
+class Dog:
+  species = "Canis familiaris"  
+  def __init__(self, name):
+    self.name = name 
 
+  @classmethod
+  def get_species(cls):
+    return cls.species
+print(Dog.get_species())
