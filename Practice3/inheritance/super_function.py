6b8ef4e1534e33fb41274c8e2cb337c0f7e0655b
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-19 01:05:13 +0500

    Update Student class to accept graduation year
    Refactored Student class to include graduation year as a parameter.

--- Practice3/inheritance/super_function.py
+++ Practice3/inheritance/super_function.py
@@ -1,1 +1,18 @@
+#1
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
 
+#2
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+#3
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 201
