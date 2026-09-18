d63d016fafdf5cdd03cec7c195bd6488e6f9a46f
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-19 00:46:10 +0500

    Add function definitions and temperature conversion
    Added multiple function definitions and calls, including a conversion function from Fahrenheit to Celsius.

--- Practice3/functions/basic_functions.py
+++ Practice3/functions/basic_functions.py
@@ -1,1 +1,25 @@
+#1
+def my_function():
+  print("Hello from a function")
+  
+#2
+def my_function():
+  print("Hello from a function")
 
+my_function()
+
+#3
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+#4
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
