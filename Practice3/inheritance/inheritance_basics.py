b282f8a35c5d8b8f5824313478b8ee565956724c
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-19 01:03:58 +0500

    Update inheritance_basics.py

--- Practice3/inheritance/inheritance_basics.py
+++ Practice3/inheritance/inheritance_basics.py
@@ -1,1 +1,19 @@
+#1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("John", "Doe")
+x.printname()
+
+#2
+class Student(Person):
+  pass
+
+#3
+x = Student("Mike", "Olsen")
+x.printname()
