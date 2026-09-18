b98e9e081c43a391f3253f95f97f7b3a5b81d08c
dilya <dilnarauzakbayeva140@gmail.com>
2026-09-19 00:54:48 +0500

    Update Person instance with new name and age

--- Practice3/classes/init_method.py
+++ Practice3/classes/init_method.py
@@ -1,1 +1,23 @@
+#1
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#2
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 28)
+
+print(p1.name)
+print(p1.age)
+
 
